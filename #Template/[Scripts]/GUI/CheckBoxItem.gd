67732be51fa5a71169c0a270bb30aa321effb5dc
meny2333 <220040994+meny2333@users.noreply.github.com>
2026-05-30 08:26:06 +0800

    feat: implement CheckBoxItem toggle control

---
+++ #Template/[Scripts]/GUI/CheckBoxItem.gd
@@ -0,0 +1,34 @@
+@tool
+extends HBoxContainer
+class_name CheckBoxItem
+
+signal toggled(is_on: bool)
+
+var checkbox: CheckBox
+var label: Label
+
+func _init(p_title: String = ""):
+	_init_ui(p_title)
+
+func _init_ui(p_title: String) -> void:
+	checkbox = CheckBox.new()
+	checkbox.flat = true
+	add_child(checkbox)
+	checkbox.toggled.connect(_on_toggled)
+
+	label = Label.new()
+	label.text = p_title
+	label.add_theme_font_size_override("font_size", 14)
+	add_child(label)
+
+func set_title(text: String) -> void:
+	label.text = text
+
+func set_is_on(value: bool) -> void:
+	checkbox.button_pressed = value
+
+func get_is_on() -> bool:
+	return checkbox.button_pressed
+
+func _on_toggled(is_on: bool) -> void:
+	emit_signal("toggled", is_on)
